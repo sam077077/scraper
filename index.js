cc9712e1779098e8e2fe6bc546fa1bff343b716c
sam077077 <gublu97844444@gmail.com>
2025-07-25 13:06:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 (async () => {
+  const executablePath = await chromium.executablePath;
+
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: executablePath,
+    headless: chromium.headless
   });
+
   const page = await browser.newPage();
 
   await page.goto("https://www.binance.com/en/earn/flexible", {
